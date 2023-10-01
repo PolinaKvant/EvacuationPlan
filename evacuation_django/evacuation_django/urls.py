29ad6321073a782665a2ca72abc844d6e01f3449
morevq <zhenuli5@gmail.com>
2023-10-01 12:45:26 +0300

    last commit

--- evacuation_django/evacuation_django/urls.py
+++ evacuation_django/evacuation_django/urls.py
@@ -23,5 +23,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index_page),
-    path('plan/', views.plan),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
